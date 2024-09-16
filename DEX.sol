0ec0fba75ea8c7b3ee7e3079eb0c828406b57e33
Raj Lathigra <63877323+raj-1106@users.noreply.github.com>
2024-09-16 17:59:57 +0530

    Updated DEX.sol

--- DEX.sol
+++ DEX.sol
@@ -1,88 +1,133 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./dex.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract DEXTest {
-    DEX public dex;
-    TestToken public token0;
-    TestToken public token1;
-    uint256 constant INITIAL_SUPPLY = 1000000 * 10**18;
+contract DEX {
+    using SafeMath for uint256;
 
-    event TestResult(string testName, bool passed);
-
-    constructor() {
-        dex = new DEX();
-        token0 = new TestToken("Token0", "TKN0", INITIAL_SUPPLY);
-        token1 = new TestToken("Token1", "TKN1", INITIAL_SUPPLY);
+    struct Pool {
+        IERC20 token0;
+        IERC20 token1;
+        uint256 reserve0;
+        uint256 reserve1;
+        uint256 totalSupply;
     }
 
-    function testAddLiquidity() public {
-        uint256 amount0 = 1000 * 10**18;
-        uint256 amount1 = 2000 * 10**18;
+    mapping(address => mapping(address => Pool)) public pools;
+    mapping(address => mapping(address => mapping(address => uint256))) public liquidity;
 
-        token0.approve(address(dex), amount0);
-        token1.approve(address(dex), amount1);
+    event LiquidityAdded(address indexed user, address indexed token0, address indexed token1, uint256 amount0, uint256 amount1, uint256 liquidityMinted);
+    event LiquidityRemoved(address indexed user, address indexed token0, address indexed token1, uint256 amount0, uint256 amount1, uint256 liquidityBurned);
+    event Swap(address indexed user, address indexed tokenIn, address indexed tokenOut, uint256 amountIn, uint256 amountOut);
 
-        dex.addLiquidity(address(token0), address(token1), amount0, amount1);
-
-        (uint256 reserve0, uint256 reserve1) = dex.getReserves(address(token0), address(token1));
-
-        emit TestResult("AddLiquidity", reserve0 == amount0 && reserve1 == amount1);
+    function sqrt(uint256 y) internal pure returns (uint256 z) {
+        if (y > 3) {
+            z = y;
+            uint256 x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
     }
 
-    function testSwap() public {
-        uint256 swapAmount = 100 * 10**18;
-        token0.approve(address(dex), swapAmount);
+    function addLiquidity(address _token0, address _token1, uint256 _amount0, uint256 _amount1) external {
+        require(_token0 != _token1, "Invalid token pair");
+        require(_amount0 > 0 && _amount1 > 0, "Invalid amounts");
 
-        uint256 initialBalance = token1.balanceOf(address(this));
-        dex.swap(address(token0), address(token1), swapAmount);
-        uint256 finalBalance = token1.balanceOf(address(this));
+        IERC20 token0 = IERC20(_token0);
+        IERC20 token1 = IERC20(_token1);
 
-        emit TestResult("Swap", finalBalance > initialBalance);
+        token0.transferFrom(msg.sender, address(this), _amount0);
+        token1.transferFrom(msg.sender, address(this), _amount1);
+
+        Pool storage pool = pools[_token0][_token1];
+        uint256 liquidityMinted;
+
+        if (pool.totalSupply == 0) {
+            liquidityMinted = sqrt(_amount0.mul(_amount1));
+            pool.token0 = token0;
+            pool.token1 = token1;
+        } else {
+            uint256 liquidity0 = _amount0.mul(pool.totalSupply) / pool.reserve0;
+            uint256 liquidity1 = _amount1.mul(pool.totalSupply) / pool.reserve1;
+            liquidityMinted = (liquidity0 < liquidity1) ? liquidity0 : liquidity1;
+        }
+
+        require(liquidityMinted > 0, "Insufficient liquidity minted");
+
+        pool.reserve0 = pool.reserve0.add(_amount0);
+        pool.reserve1 = pool.reserve1.add(_amount1);
+        pool.totalSupply = pool.totalSupply.add(liquidityMinted);
+        liquidity[msg.sender][_token0][_token1] = liquidity[msg.sender][_token0][_token1].add(liquidityMinted);
+
+        emit LiquidityAdded(msg.sender, _token0, _token1, _amount0, _amount1, liquidityMinted);
     }
 
-    function testRemoveLiquidity() public {
-        uint256 amount0 = 1000 * 10**18;
-        uint256 amount1 = 2000 * 10**18;
+    function removeLiquidity(address _token0, address _token1, uint256 _liquidityBurned) external {
+        require(_token0 != _token1, "Invalid token pair");
+        require(_liquidityBurned > 0, "Invalid liquidity amount");
 
-        token0.approve(address(dex), amount0);
-        token1.approve(address(dex), amount1);
+        Pool storage pool = pools[_token0][_token1];
+        require(pool.totalSupply > 0, "Pool does not exist");
+        require(liquidity[msg.sender][_token0][_token1] >= _liquidityBurned, "Insufficient liquidity");
 
-        dex.addLiquidity(address(token0), address(token1), amount0, amount1);
+        uint256 amount0 = _liquidityBurned.mul(pool.reserve0) / pool.totalSupply;
+        uint256 amount1 = _liquidityBurned.mul(pool.reserve1) / pool.totalSupply;
 
-        uint256 liquidityToRemove = dex.liquidity(address(this), address(token0), address(token1));
+        require(amount0 > 0 && amount1 > 0, "Insufficient liquidity burned");
 
-        uint256 initialBalance0 = token0.balanceOf(address(this));
-        uint256 initialBalance1 = token1.balanceOf(address(this));
+        liquidity[msg.sender][_token0][_token1] = liquidity[msg.sender][_token0][_token1].sub(_liquidityBurned);
+        pool.totalSupply = pool.totalSupply.sub(_liquidityBurned);
+        pool.reserve0 = pool.reserve0.sub(amount0);
+        pool.reserve1 = pool.reserve1.sub(amount1);
 
-        dex.removeLiquidity(address(token0), address(token1), liquidityToRemove);
+        pool.token0.transfer(msg.sender, amount0);
+        pool.token1.transfer(msg.sender, amount1);
 
-        uint256 finalBalance0 = token0.balanceOf(address(this));
-        uint256 finalBalance1 = token1.balanceOf(address(this));
-
-        emit TestResult("RemoveLiquidity", finalBalance0 > initialBalance0 && finalBalance1 > initialBalance1);
+        emit LiquidityRemoved(msg.sender, _token0, _token1, amount0, amount1, _liquidityBurned);
     }
 
-    function testGetReserves() public {
-        uint256 amount0 = 1000 * 10**18;
-        uint256 amount1 = 2000 * 10**18;
+    function swap(address _tokenIn, address _tokenOut, uint256 _amountIn) external {
+        require(_tokenIn != _tokenOut, "Invalid token pair");
+        require(_amountIn > 0, "Invalid input amount");
 
-        token0.approve(address(dex), amount0);
-        token1.approve(address(dex), amount1);
+        Pool storage pool = pools[_tokenIn][_tokenOut];
+        require(pool.totalSupply > 0, "Pool does not exist");
 
-        dex.addLiquidity(address(token0), address(token1), amount0, amount1);
+        IERC20 tokenIn = IERC20(_tokenIn);
+        IERC20 tokenOut = IERC20(_tokenOut);
 
-        (uint256 reserve0, uint256 reserve1) = dex.getReserves(address(token0), address(token1));
+        tokenIn.transferFrom(msg.sender, address(this), _amountIn);
 
-        emit TestResult("GetReserves", reserve0 == amount0 && reserve1 == amount1);
+        uint256 amountInWithFee = _amountIn.mul(997);
+        uint256 numerator = amountInWithFee.mul(pool.reserve1);
+        uint256 denominator = pool.reserve0.mul(1000).add(amountInWithFee);
+        uint256 amountOut = numerator / denominator;
+
+        require(amountOut > 0, "Insufficient output amount");
+
+        pool.reserve0 = pool.reserve0.add(_amountIn);
+        pool.reserve1 = pool.reserve1.sub(amountOut);
+
+        tokenOut.transfer(msg.sender, amountOut);
+
+        emit Swap(msg.sender, _tokenIn, _tokenOut, _amountIn, amountOut);
     }
 
-    function runAllTests() public {
-        testAddLiquidity();
-        testSwap();
-        testRemoveLiquidity();
-        testGetReserves();
+    function getReserves(address _token0, address _token1) external view returns (uint256 reserve0, uint256 reserve1) {
+        Pool storage pool = pools[_token0][_token1];
+        return (pool.reserve0, pool.reserve1);
+    }
+}
+
+contract TestToken is ERC20 {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
     }
 }
